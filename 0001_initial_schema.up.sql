cc351f6e5b9123f2734a9fb4109747067e81e07a
jmuru <jmuruguru94@gmail.com>
2022-02-07 15:00:14 -0500

    fix migrations

--- 0001_initial_schema.up.sql
+++ 0001_initial_schema.up.sql
@@ -1,31 +1,8 @@
 
-
-CREATE TABLE IF NOT EXISTS Clothes (
-    id SERIAL PRIMARY KEY,
-    asset_url VARCHAR UNIQUE NOT NULL,
-    category VARCHAR NOT NULL,
-    created_at TIMESTAMP,
-    created_by BIGINT UNIQUE NOT NULL,
-    brand VARCHAR,
-    FOREIGN KEY (created_by) REFERENCES Users(id),
-)
-
-CREATE TABLE IF NOT EXISTS Outfits (
-    id SERIAL PRIMARY KEY
-    outfit_id INT NOT NULL,
-    created_by BIGINT UNIQUE NOT NULL,
-    created_at TIMESTAMP,
-    last_modified TIMESTAMP,
-    item_id BIGINT UNIQUE NOT NULL,
-    FOREIGN KEY (created_by) REFERENCES Users(id),
-    FOREIGN KEY (item_id) REFERENCES Clothes(id),
-)
-
-
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY NOT NULL,
     username VARCHAR NOT NULL UNIQUE,
-    password_hash BINARY(64) NOT NULL,
+    password_hash VARCHAR NOT NULL,
     first_name VARCHAR,
     last_name VARCHAR,
     location_city VARCHAR,
@@ -40,15 +17,33 @@ CREATE TABLE IF NOT EXISTS Users (
     pinterest VARCHAR,
     last_login TIMESTAMP,
     registered_at TIMESTAMP,
-    closet_id INT,
-)
+    closet_id INT);
 
-CREATE TABLE IF NOT EXISTS Closets (
+CREATE TABLE IF NOT EXISTS clothes(
+    id SERIAL PRIMARY KEY,
+    asset_url VARCHAR UNIQUE NOT NULL,
+    category VARCHAR NOT NULL,
+    created_at TIMESTAMP,
+    created_by BIGINT UNIQUE NOT NULL,
+    brand VARCHAR,
+    FOREIGN KEY (created_by) REFERENCES Users(id));
+
+CREATE TABLE IF NOT EXISTS outfits(
+    id SERIAL PRIMARY KEY,
+    outfit_id INT NOT NULL,
+    created_by BIGINT UNIQUE NOT NULL,
+    created_at TIMESTAMP,
+    last_modified TIMESTAMP,
+    item_id BIGINT UNIQUE NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES Users(id),
+    FOREIGN KEY (item_id) REFERENCES Clothes(id));
+
+CREATE TABLE IF NOT EXISTS closets(
     id SERIAL PRIMARY KEY NOT NULL,
     is_character BOOLEAN,
     last_modified TIMESTAMP,
     created_at TIMESTAMP,
     outfit_id INT,
-    FOREIGN KEY (closet_owner) REFERENCES Users(id),
-)
+    closet_owner INT,
+    FOREIGN KEY (closet_owner) REFERENCES Users(id));
 
